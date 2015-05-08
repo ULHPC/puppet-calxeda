b9e514da92367b9f4deda7611c7d7ab1b99cdf52
Valentin Plugaru <valentin.plugaru@uni.lu>
2015-05-08 15:37:36 +0200

    update content of 'params.pp'
    Signed-off-by: Valentin Plugaru <valentin.plugaru@uni.lu>

--- tests/params.pp
+++ tests/params.pp
@@ -15,14 +15,17 @@
 
 include 'calxeda::params'
 
-$names = ['ensure', 'protocol', 'port', 'packagename']
+$names = ["ensure", "ipmi_git", "ipmi_dep", "build_dep", "ipmi_build_dir", "cxmanage_dep", "cxmanage_name"]
 
 notice("calxeda::params::ensure = ${calxeda::params::ensure}")
-notice("calxeda::params::protocol = ${calxeda::params::protocol}")
-notice("calxeda::params::port = ${calxeda::params::port}")
-notice("calxeda::params::packagename = ${calxeda::params::packagename}")
+notice("calxeda::params::ipmi_git = ${calxeda::params::ipmi_git}")
+notice("calxeda::params::ipmi_dep = ${calxeda::params::ipmi_dep}")
+notice("calxeda::params::build_dep = ${calxeda::params::build_dep}")
+notice("calxeda::params::ipmi_build_dir = ${calxeda::params::ipmi_build_dir}")
+notice("calxeda::params::cxmanage_dep = ${calxeda::params::cxmanage_dep}")
+notice("calxeda::params::cxmanage_name = ${calxeda::params::cxmanage_name}")
 
 #each($names) |$v| {
-#    $var = "ULHPC/calxeda::params::${v}"
+#    $var = "calxeda::params::${v}"
 #    notice("${var} = ", inline_template('<%= scope.lookupvar(@var) %>'))
 #}
